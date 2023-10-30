b5a83c648a8de013a0e3fd1a3dcae253260bf09a
ehuntober <ehuntercrypto@gmail.com>
2023-10-30 11:55:38 +0000

    modified server file

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-// app.js
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const paymentController = require('./controllers/paymentController');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 app.use(bodyParser.json());
 
-app.post('/process-payment', paymentController.processPayment);
-app.get('/payments', paymentController.getPayments);
+// Routes
+const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
 
+app.use('/auth', authRoutes);
+app.use('/admin', adminRoutes);
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
